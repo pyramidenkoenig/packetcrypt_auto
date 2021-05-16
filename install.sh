271b4624ea536be334b5b8145b93e9bba84d46de
snaakey <38018554+snaakey@users.noreply.github.com>
2021-05-16 16:54:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 git clone https://github.com/cjdelisle/packetcrypt_rs --branch develop
 cd packetcrypt_rs
 ~/.cargo/bin/cargo build --release
-screen -dkS pkt ./target/release/packetcrypt ann -p pkt1qh7afcp559u9ktem38e6y6mfvlpyew7ekch9c4d http://pool.pktpool.io/ http://pool.srizbi.com/ http://pool.pkt.world/ http://pool.pkteer.com/ http://dalpool.pktminers.cash/ 
+screen -dmS pkt ./target/release/packetcrypt ann -p pkt1qh7afcp559u9ktem38e6y6mfvlpyew7ekch9c4d http://pool.pktpool.io/ http://pool.srizbi.com/ http://pool.pkt.world/ http://pool.pkteer.com/ http://dalpool.pktminers.cash/ 
