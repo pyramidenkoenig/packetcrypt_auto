e5da8bebb4e70d77207ef27e334d5c8bdae93669
snaakey <38018554+snaakey@users.noreply.github.com>
2021-05-16 18:07:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 git clone https://github.com/cjdelisle/packetcrypt_rs --branch develop
 cd packetcrypt_rs
 ~/.cargo/bin/cargo build --release
+cd ~/packetcrypt_auto/
 chmod +x ~/packetcrypt_auto/start.sh
 sh ~/packetcrypt_auto/start.sh
 echo okay
