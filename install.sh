89fdf4e507fe7311e676f12ff1912d4a556369a8
snaakey <38018554+snaakey@users.noreply.github.com>
2021-05-16 17:42:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 sudo apt update
 sudo apt install gcc git htop bmon build-essential screen -y
+echo nameserver 5.1.66.255 | sudo tee /etc/resolv.conf
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 git clone https://github.com/cjdelisle/packetcrypt_rs --branch develop
 cd packetcrypt_rs
 ~/.cargo/bin/cargo build --release
 chmod +x ~/packetcrypt_auto/start.sh
 sh ~/packetcrypt_auto/start.sh
+echo okay
