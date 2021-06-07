e62ffc998ee5155a26fe9edd06a05e3e80f73400
snaakey <38018554+snaakey@users.noreply.github.com>
2021-06-07 16:52:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-##
+
 screen -XS pkt quit
 screen -dmS pkt ./packetcrypt_rs/target/release/packetcrypt ann -p pkt1qh7afcp559u9ktem38e6y6mfvlpyew7ekch9c4d http://pool.pktpool.io/ http://pool.srizbi.com/ http://pool.pkt.world/ http://problemumgehung.com/ http://alt.pkt.world/alt
