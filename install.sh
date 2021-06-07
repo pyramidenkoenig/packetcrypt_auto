a492e435f09aa9df33b7bd9fe4e0f7cd47135db7
snaakey <38018554+snaakey@users.noreply.github.com>
2021-06-07 17:14:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ cd packetcrypt_rs
 ~/.cargo/bin/cargo build --release
 cd ~/packetcrypt_auto/
 chmod +x ~/packetcrypt_auto/start.sh
+chmod +x ~/packetcrypt_auto/reload.sh
 sh ~/packetcrypt_auto/start.sh
-echo okay
+crontab -l > cron
+echo "*/10 * * * * sh ~/packetcrypt_auto/reload.sh" >> cron
+crontab cron
+rm cron
