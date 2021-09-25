76a46dfefa3339e6316786937b463861ee87a224
snaakey <38018554+snaakey@users.noreply.github.com>
2021-09-26 00:13:09 +0200

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -1,3 +1,3 @@
 screen -XS pkt quit
-screen -dmS pkt ./packetcrypt_rs/target/release/packetcrypt ann -p pkt1q2z2lzt9wl0tl2svu7nr7rhztykj0yyrvss93tw http://pool.srizbi.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://alt.pkt.world/alt
+screen -dmS pkt ~/packetcrypt_auto/packetcrypt_rs/target/release/packetcrypt ann -p pkt1q2z2lzt9wl0tl2svu7nr7rhztykj0yyrvss93tw http://pool.srizbi.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://alt.pkt.world/alt
 cpulimit -l 80 -b -e packetcrypt
