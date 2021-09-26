7832c7919abc9180ecd9b5fc3452bd3705684ef6
snaakey <38018554+snaakey@users.noreply.github.com>
2021-09-26 15:52:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ git clone https://github.com/cjdelisle/packetcrypt_rs --branch develop
 cd packetcrypt_rs
 ~/.cargo/bin/cargo build --release
 cd ~/packetcrypt_auto/
-chmod +x ~/packetcrypt_auto/start1.sh && chmod +x ~/packetcrypt_auto/start2.sh
+chmod +x ~/packetcrypt_auto/start.sh && chmod +x ~/packetcrypt_auto/start2.sh
 #chmod +x ~/packetcrypt_auto/reload.sh
-sh ~/packetcrypt_auto/start1.sh
+sh ~/packetcrypt_auto/start.sh
 #crontab -l > cron
 #echo "@reboot sh ~/packetcrypt_auto/start2.sh" >> cron
 #crontab cron
