f82ef8f0759e57e95fc1e8cc48dd26fd70abe2fc
pyramidenkoenig <44556055+pyramidenkoenig@users.noreply.github.com>
2022-02-15 00:12:37 +0100

    Update start.sh
    Change Florian Brandstetter for Zetahash

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 screen -XS pkt quit
-screen -dmS pkt ./packetcrypt_rs/target/release/packetcrypt ann -p pkt1q2z2lzt9wl0tl2svu7nr7rhztykj0yyrvss93tw http://pool.srizbi.com/ http://pool.pkt.world/ http://pool.pktpool.io/
+screen -dmS pkt ./packetcrypt_rs/target/release/packetcrypt ann -p pkt1q2z2lzt9wl0tl2svu7nr7rhztykj0yyrvss93tw https://stratum.zetahash.com/ http://pool.pkt.world/ http://pool.pktpool.io/
